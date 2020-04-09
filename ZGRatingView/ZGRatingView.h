ac1456f4169cb6574d536723f5a6bd82c9c11086
Zyad_Galal <zyad_mg@yahoo.com>
2020-04-09 15:37:02 +0200

    Initial Commit

---
+++ ZGRatingView/ZGRatingView.h
@@ -0,0 +1,19 @@
+//
+//  ZGRatingView.h
+//  ZGRatingView
+//
+//  Created by Zyad Galal on 4/9/20.
+//  Copyright © 2020 Zyad Galal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZGRatingView.
+FOUNDATION_EXPORT double ZGRatingViewVersionNumber;
+
+//! Project version string for ZGRatingView.
+FOUNDATION_EXPORT const unsigned char ZGRatingViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZGRatingView/PublicHeader.h>
+
+
